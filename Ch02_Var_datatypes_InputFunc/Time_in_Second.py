53d0dcfa88d6b8f7ec7265866884512370110624
prasunNstu <prasunjeetsaha@gmail.com>
2025-07-28 20:59:21 +0600

    Added Time in seconds Problem

---
+++ Ch02_Var_datatypes_InputFunc/Time_in_Second.py
@@ -0,0 +1,5 @@
+Hours=int(input("Enter the number of hours: ") )
+Minutes=int(input("Enter the number of minutes: ") )
+Seconds=int(input("Enter the number of seconds: ") )    
+Total_seconds = (Hours * 3600) + (Minutes * 60) + Seconds
+print("Total time in seconds is:", Total_seconds)  # Ensure the output is an integer5
