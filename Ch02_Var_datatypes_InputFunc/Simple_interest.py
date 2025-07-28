994997bd02f16b4ce0e86b38e137be331bd6e0a5
prasunNstu <prasunjeetsaha@gmail.com>
2025-07-28 20:49:00 +0600

    Added Simple Interest count problem

---
+++ Ch02_Var_datatypes_InputFunc/Simple_interest.py
@@ -0,0 +1,5 @@
+a=int(input("Enter a value of Principal: "))
+b=int(input("Enter a value of interest Rate: "))
+c=int(input("Enter a value of Time in years: "))
+d=(a*b*c)/100 #simple interest formula: I=pnr, r=q%/100
+print("The Simple Interest is:", d)  
