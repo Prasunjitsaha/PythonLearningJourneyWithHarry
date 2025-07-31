ded740942ba2b46648e7da5e89c22b33bd5ac1d1
prasunNstu <prasunjeetsaha@gmail.com>
2025-07-31 12:53:59 +0600

    Added negative Slicing code

---
+++ Ch03_Strings/Neg_slicing.py
@@ -0,0 +1,7 @@
+nm="Prasunjit"
+nm2=nm[-9:-1]
+nm3=nm[-9:-1]  #length-9=0 , length-1=8 so it will slice from index 0 to index 8
+nm4=nm[-4:-2] #length-4=5 , length-2=7 so it will slice from index 5 to index 7
+print(nm2)
+print(nm3)
+print(nm4)
